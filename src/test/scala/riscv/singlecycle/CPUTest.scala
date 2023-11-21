d60b4741f0d8bcce97cf0ccebb140948030dabf2
eeeXun <sdes96303@gmail.com>
2023-11-21 22:11:48 +0800

    Add HW2Test
    Remove ecall in hw2.S to make it runnable in current CPU

--- src/test/scala/riscv/singlecycle/CPUTest.scala
+++ src/test/scala/riscv/singlecycle/CPUTest.scala
@@ -113,3 +113,17 @@ class ByteAccessTest extends AnyFlatSpec with ChiselScalatestTester {
     }
   }
 }
+
+class HW2Test extends AnyFlatSpec with ChiselScalatestTester {
+  behavior.of("Single Cycle CPU")
+  it should "multiply two bfloat16" in {
+    test(new TestTopModule("hw2.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+      for (i <- 1 to 50) {
+        c.clock.step(1000)
+        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+      }
+      c.io.regs_debug_read_address.poke(19.U) // s3
+      c.io.regs_debug_read_data.expect(0x440a0000.U)
+    }
+  }
+}
